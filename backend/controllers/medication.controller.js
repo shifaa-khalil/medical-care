a99ce1f77916637dbbd8d8009255006e28df8684
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-05-26 19:08:20 +0300

    implemented drop medication API

--- backend/controllers/medication.controller.js
+++ backend/controllers/medication.controller.js
@@ -1,5 +1,4 @@
 const Medication = require("../models/medication.model");
-
 exports.addMedication = async (req, res) => {
   try {
     const medication = new Medication({
@@ -19,4 +18,18 @@ exports.addMedication = async (req, res) => {
     console.error(error);
     res.status(500).json({ error: "Could not add medication" });
   }
+};
+
+exports.dropMedication = async (req, res) => {
+  Medication.findOneAndDelete({ _id: req.params.medication_id })
+    .exec()
+    .then((droppedMedication) => {
+      if (droppedMedication)
+        res.json({ message: "medication dropped", droppedMedication });
+      else res.json({ message: "medication not found" });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    });
 };
--- backend/routes/medication.routes.js
+++ backend/routes/medication.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
-const { addMedication } = require("../controllers/medication.controller");
+const {
+  addMedication,
+  dropMedication,
+} = require("../controllers/medication.controller");
 
 const router = Router();
 
 router.post("/medication/:patient_id", addMedication);
+router.delete("/medication/:medication_id", dropMedication);
 
 module.exports = router;
