2bd2bc0c309c41682be46f780b6504f850845239
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-05-26 18:25:36 +0300

    implemented drop patient API

--- backend/controllers/patient.controller.js
+++ backend/controllers/patient.controller.js
@@ -11,3 +11,17 @@ exports.getPatients = async (req, res) => {
       res.status(500).json({ message: "Internal server error" });
     });
 };
+
+exports.dropPatient = async (req, res) => {
+  User.findOneAndDelete({ _id: req.params.patient_id })
+    .exec()
+    .then((droppedPatient) => {
+      if (droppedPatient)
+        res.json({ message: "patient dropped", droppedPatient });
+      else res.json({ message: "patient not found" });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    });
+};
--- backend/routes/patient.routes.js
+++ backend/routes/patient.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
-const { getPatients } = require("../controllers/patient.controller");
+const {
+  getPatients,
+  dropPatient,
+} = require("../controllers/patient.controller");
 
 const router = Router();
 
 router.get("/patients", getPatients);
+router.delete("/patient/:patient_id", dropPatient);
 
 module.exports = router;
